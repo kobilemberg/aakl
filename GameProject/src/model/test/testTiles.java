d0629d9915ea74da47197d1e652e04b53ba40532
alon-abadi <alon.abadi@gmail.com>
2015-01-02 16:53:05 +0200

    new tester

---
+++ GameProject/src/model/test/testTiles.java
@@ -0,0 +1,13 @@
+package model.test;
+
+import model.domains.tiles.TilesSearchDomain;
+
+public class testTiles {
+
+	public static void main(String[] args) {
+
+		TilesSearchDomain tsd = new TilesSearchDomain(4);
+		System.out.println("\nDone.");
+	}
+
+}
