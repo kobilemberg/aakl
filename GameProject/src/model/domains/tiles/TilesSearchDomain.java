90e2bfdc5e84f5e28798123b0dc467a356702d5a
alon-abadi <alon.abadi@gmail.com>
2015-01-03 12:01:14 +0200

    Redo of the last version.

--- GameProject/src/model/domains/tiles/TilesSearchDomain.java
+++ GameProject/src/model/domains/tiles/TilesSearchDomain.java
@@ -1,35 +1,44 @@
 package model.domains.tiles;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import model.algorithems.Action;
-import model.algorithems.CommonState;
 import model.algorithems.SearchDomain;
 import model.algorithems.State;
 
 public class TilesSearchDomain implements SearchDomain {
 	
-	int [] matrix;
-	private int size=4;
+	Integer[] matrix;
+	private int size;
 	private CommonTilesState startState;
 	private CommonTilesState goalState;
+	
 	public TilesSearchDomain(int size) {
+		setSize(size);
 		this.setStartState(new CommonTilesState("StartState", 0, 0));
-		
 		this.startState.printMatrix();
 		System.out.println("");
 		this.goalState = new CommonTilesState("GoalState", 0, 0);
 		this.goalState.setMatrix(buildGoalMatrix());
 		this.goalState.printMatrix();
+		getActions(startState);
+		
+		
 	}
 
 
-	public int[] buildMatrix()
+	public int getSize() {
+		return size;
+	}
+
+
+	public Integer[] buildMatrix()
 	{
 		
 		boolean canBeSolved = false;
-		int[] matrix = buildGoalMatrix();
+		Integer[] matrix = buildGoalMatrix();
 		while (canBeSolved == false)
 		{
 			/* mess up the matrix except the last one (00) */
@@ -48,8 +57,8 @@ public class TilesSearchDomain implements SearchDomain {
 		return matrix;
 	}
 
-	public int[] buildGoalMatrix(){
-		int[] matrix= new int[size*size];
+	public Integer[] buildGoalMatrix(){
+		Integer[] matrix= new Integer[size*size];
 		/* create the matrix and fill it with the numbers. */
 		for (int i = 1; i < matrix.length; i++) {
 			matrix[i-1] = i;
@@ -57,7 +66,7 @@ public class TilesSearchDomain implements SearchDomain {
 		matrix[this.size*this.size-1] = 0;
 		return matrix;
 	}
-	public boolean checkIfSolveable(int[] matrix){
+	public boolean checkIfSolveable(Integer[] matrix){
 		boolean canBeSolved = false;
 		
 		int sum = evaluateMatrix(matrix);
@@ -69,7 +78,7 @@ public class TilesSearchDomain implements SearchDomain {
 		return canBeSolved;
 	}
 	
-	public int evaluateMatrix(int[] matrix)
+	public int evaluateMatrix(Integer[] matrix)
 	{
 		int sum = 0;
 		
@@ -100,9 +109,26 @@ public class TilesSearchDomain implements SearchDomain {
 		this.size = size;
 	}
 
+	public Integer[] getBlankLocation(CommonTilesState state){
+		
+		int location = Arrays.asList(state.getMatrix()).indexOf(0);
+		Integer[] result = {location % this.size,location / this.size};
+		return result;
+	}
 	public ArrayList<Action> getActions(State state) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Action> actionToRet = new ArrayList<Action>();
+		Integer x = getBlankLocation((CommonTilesState)state)[0];
+		Integer y = getBlankLocation((CommonTilesState)state)[1];
+		System.out.println("x:"+x+" y:"+y);
+		if ((x-1)>=0){
+			if((y-1)>=0){
+				actionToRet.add(action);
+			}
+		}
+		
+		
+		
+		return actionToRet;
 	}
 
 	public String getProblemDescription() {
